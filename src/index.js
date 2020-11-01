9ec2bc61363e228aefb2bd695acc00f68da526a9
johanneshermans <johannes.hermans@student.howest.be>
2020-11-01 14:42:39 +0100

    werkende orderlist

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import coffees from './coffees.json';
 
 {
   const $priceList = document.querySelector(`.prices__list`);
+  const $allOrders = document.querySelector(`.all__orders`);
   const allDrinks = [];
+  let currentDrinkList = [];
 
+  const makeNum = number => {
+    const removeSpace = number.replace(/\s/g, '');
+    const entryArray = removeSpace.split("");
+    const removeSymbol = entryArray.splice(1, 5);
+    const toSring = removeSymbol.join("");
+    return parseFloat(toSring);
+  };
 
   const JSONdata = () => {
     const coffee = coffees.coffees;
@@ -28,10 +37,37 @@ import coffees from './coffees.json';
   };
 
   const getClickedDrink = e => {
+    currentDrinkList = [];
     const currentDrink = e.currentTarget.querySelector(`.price__button__name`).innerHTML;
     const currentPrice = e.currentTarget.querySelector(`.price__button__amount`).innerHTML;
-    console.log(currentDrink);
-    console.log(currentPrice);
+    addCurrentDrink(currentDrink, makeNum(currentPrice));
+  };
+
+
+
+  const addCurrentDrink = (drink, price) => {
+    const drinkArray = [];
+    drinkArray.push(drink);
+    drinkArray.push(price);
+    currentDrinkList.push(drinkArray);
+    printCurrentDrinks();
+  };
+
+  const printCurrentDrinks = () => {
+
+    currentDrinkList.forEach(drink => {
+      $allOrders.innerHTML += `<ul class="orders">
+            <li class="order">
+              <span class="order__name">
+                <span class="order__amount"> x</span> ${drink[0]}
+              </span>
+              <span class="order__price">&euro; ${drink[1]}</span>
+              <button class="remove">
+                x
+              </button>
+            </li>
+          </ul>`;
+    });
   };
 
 
